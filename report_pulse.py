12338b49e96ebf2a03344ec864f015d9e0a72177
Asim Shaikh <asim.shaikh@netcorecloud.com>
2023-06-04 01:40:12 +0530

    Code sanitised

--- report_pulse.py
+++ report_pulse.py
@@ -16,21 +16,12 @@ path = os.path.dirname(__file__)
 with open(path+"/Assets/translations.json") as f:
     transl = json.load(f)
 
+# Trick to preserve the state of your widgets across pages
+for k, v in st.session_state.items():
+    st.session_state[k] = v 
+##
+
 os.environ["OPENAI_API_KEY"] = st.secrets["openai_api_key"]
-
-st.title('Report pulse ChatBot')
-
-file_uploaded = st.file_uploader(label="Upload your report here")
-
-styl = f"""
-<style>
-    .stTextInput {{
-      position: fixed;
-      bottom: 3rem;
-    }}
-</style>
-"""
-st.markdown(styl, unsafe_allow_html=True)
 
 # Add the language selection dropdown    
 if 'lang_tmp' not in st.session_state:
@@ -52,9 +43,23 @@ if lang != st.session_state['lang_tmp']:
 else:
     st.session_state['lang_changed'] = False
 
+st.title(transl[lang]["title"])
+
+file_uploaded = st.file_uploader(label=transl[lang]["title"])
+
+styl = f"""
+<style>
+    .stTextInput {{
+      position: fixed;
+      bottom: 3rem;
+    }}
+</style>
+"""
+st.markdown(styl, unsafe_allow_html=True)
+
 if file_uploaded is not None:
     def display_messages():
-        st.subheader("Chat")
+        
         for i, (msg, is_user) in enumerate(st.session_state["messages"]):
             message(msg, is_user=is_user, key=str(i))
         st.session_state["thinking_spinner"] = st.empty()
@@ -70,22 +75,8 @@ if file_uploaded is not None:
             st.session_state["messages"].append((agent_text, False))
         
     # Storing the chat
-    if 'generated' not in st.session_state:
-        st.session_state['generated'] = []
-
     if 'messages' not in st.session_state:
         st.session_state['messages'] = []
-
-    if 'past' not in st.session_state:
-        st.session_state['past'] = []
-    
-    def inputchange():
-        output = generate_response(st.session_state.input)
-
-        # append user_input and output to state
-        
-        st.session_state.past.append(st.session_state.input)
-        showmessage(output)
 
     def showmessage(output):
         st.session_state["messages"].append((output, False))
@@ -101,34 +92,18 @@ if file_uploaded is not None:
 
         if save_path.exists():
             st.success(f'File {uploadedFile.name} is successfully saved!')
-        #logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
-        #logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
-
+        
         return ReportPulseAssistent(save_folder)
         
     reportPulseAgent = upload_file(file_uploaded)    
     r_response = reportPulseAgent.get_next_message().response
+    st.sidebar.markdown(r_response)
     
-    showmessage(r_response)
 
     def generate_response(user_query):
         response = reportPulseAgent.get_next_message(user_query)
         return response.response
 
-    #if user_input:
-    #   output = generate_response(user_input)
-    #    # store the output 
-    #    st.session_state.past.append(user_input)
-    #    st.session_state.generated.append(output)
-
     # We will get the user's input by calling the get_text function
-    st.text_input("You: ","Ask Question From your Document ?", key="user_input", on_change=process_input)
-    
-    
+    st.text_input(transl[lang]['ask_question'],key="user_input", on_change=process_input)
     display_messages()
-    #if st.session_state['generated']:
-    #    for i in range(len(st.session_state['generated'])-1, -1, -1):
-    #        message(st.session_state["generated"][i], key=str(i))
-    #        past_key = i-1
-    #        if past_key in st.session_state['past']:
-    #            message(st.session_state['past'][i-1], is_user=True, key=str#(i-1) + '_user')
